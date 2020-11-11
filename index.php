d2c7fee2a9af89b82aa392d537dbbd9cf33d9763
wadewadewadewadewadewade <wade.wade.wade.wade.wade.wade@gmail.com>
2020-11-10 20:57:18 -0800

    typescript compiles to browser

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+    <meta charset="utf-8"/>
     <title>Test</title>
     <link rel="stylesheet" href="/build/bundle.css"/>
   </head>
@@ -33,29 +34,34 @@ error_reporting(E_ALL);
         exit();
       }
 
-      $firstname = 'Timber';
+      $firstName = 'Timber';
       $lastName = 'Saw';
-      $query = sprintf("SELECT firstName, lastName, age FROM user 
-        WHERE firstName='%s' AND lastName='%s'",
-        $mysqli->real_escape_string($firstname),
-        $mysqli->real_escape_string($lastname));
+      $query = sprintf("SELECT firstName, lastName, age FROM user ");
+        /*WHERE firstName='%s' AND lastName='%s'",
+        $mysqli->real_escape_string($firstName),
+        $mysqli->real_escape_string($lastName));*/
 
-      $result = $mysqli->query($query, $link);
+      $result = $mysqli->query($query);
       if (!$result) {
         $message  = 'Invalid query: ' . mysql_error() . "\n";
         $message .= 'Whole query: ' . $query;
         die($message);
       }
 
+      echo "<div class=\"cards\">";
       while ($row = mysqli_fetch_assoc($result)) {
-        echo $row['firstName'];
-        echo $row['lastName'];
-        echo $row['age'];
+        echo "<div>";
+        echo "<span>{$row['firstName']}</span>";
+        echo "<span>{$row['lastName']}</span>";
+        echo "<span>{$row['age']}</span>";
+        echo "</div>";
       }
+      echo "</div>";
 
-      $mysqli=>close();
+      $mysqli->close();
 
     ?>
-    <script src="/bundle/build.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/build/bundle.js"></script>
   </body>
 </html>
--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,33 @@
-import tscc from '@tscc/rollup-plugin-tscc';
+import resolve from "@rollup/plugin-node-resolve";
+//import tscc from '@tscc/rollup-plugin-tscc';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
 //import sass from 'rollup-plugin-sass';
 import scss from './index.es.js';
+import commonjs from '@rollup/plugin-commonjs';
+import tsconfig from './tsconfig.json';
 
 export default {
+    input: 'src/app.ts',
     output: {
-        dir: 'build/'
+        file: 'build/bundle.js',
+        format: 'iife',
+        globals: {
+            'jquery': '$'
+        }
     },
+    external: ['jquery'],
     plugins: [
+        //tscc(),
         terser(),
-        tscc(),
-        typescript(),
+        resolve({
+            browser: true
+        }), // So rollup can find imports
+        commonjs(), // Convert to an ES module
+        typescript({
+            ...tsconfig.compilerOptions,
+            include: '**/*.{js,ts}'
+        }),
         scss({
             include: './src/**/*.scss',
             output: './build/bundle.css'
